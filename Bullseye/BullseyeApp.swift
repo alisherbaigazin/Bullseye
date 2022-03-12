1f584b886b687d204a60b56ac2ae03d2b1ad2c4a
alisherbaigazin <alisherbaigazin@gmail.com>
2022-03-12 14:19:13 +0600

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Alisher Baigazin on 12.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
